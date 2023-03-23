a79f04c06d22fafc07f5766519a29f23463e2ef3
disney <disney@tokeninsight.com>
2023-03-23 19:30:36 +0800

    add memory

--- chat.ts
+++ chat.ts
@@ -2,11 +2,14 @@ import dotenv from "dotenv";
 import { CallbackManager } from "langchain/callbacks";
 import { ConversationChain } from "langchain/chains";
 import { ChatOpenAI } from "langchain/chat_models";
+import { Document } from "langchain/document";
+import { OpenAIEmbeddings } from "langchain/embeddings";
 import { BufferMemory } from "langchain/memory";
 import {
   ChatPromptTemplate,
   HumanMessagePromptTemplate,
   MessagesPlaceholder,
+  PromptTemplate,
   SystemMessagePromptTemplate,
 } from "langchain/prompts";
 import {
@@ -15,6 +18,8 @@ import {
   LLMResult,
   SystemChatMessage,
 } from "langchain/schema";
+import { HNSWLib } from "langchain/vectorstores";
+import { v4 as uuidv4 } from "uuid";
 
 dotenv.config();
 
@@ -22,7 +27,7 @@ dotenv.config();
 
 const callbackManager = CallbackManager.fromHandlers({
   handleLLMNewToken: async (token: string) => {
-    console.log(token);
+    // console.log(token);
   },
   handleLLMStart: async (llm: { name: string }, prompts: string[]) => {
     console.log(JSON.stringify(llm, null, 2));
@@ -65,8 +70,34 @@ const chain = new ConversationChain({
   llm: chat,
 });
 
+// Save the vector store to a directory
+const directory = "data";
+
+// Load the vector store from the same directory
+const loadedVectorStore = await HNSWLib.load(directory, new OpenAIEmbeddings());
+
+const question = "What is pinecone?";
+
+const searchResults = await loadedVectorStore.similaritySearch("pinecone", 1);
+console.log(searchResults);
+
+const memory = searchResults[0].pageContent;
+
 const res = await chain.call({
-  input: "Write me a song about sparkling water.",
+  input: `Your memory of this conversation is: ${memory} Question: ${question}`,
 });
 
-console.log(res);
+console.log(res.response);
+
+const docs = [
+  new Document({
+    metadata: { id: uuidv4() },
+    pageContent: `Question: ${question} Answer: ${res.response}`,
+  }),
+];
+
+// console.log(docs);
+
+await loadedVectorStore.addDocuments(docs);
+
+await loadedVectorStore.save(directory);
