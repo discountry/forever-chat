e7793f48f47eb9636f963fe57d5c305af77c0deb
disney <disney@tokeninsight.com>
2023-03-27 21:06:48 +0800

    fix sync

--- chat.ts
+++ chat.ts
@@ -27,73 +27,99 @@ import { v4 as uuidv4 } from "uuid";
 
 dotenv.config();
 
-//create our callback manager
-let currentMessage = "";
-let replyedMessage = "";
+export const run = async (
+  userId: number,
+  question: string,
+  callbackManager: CallbackManager
+) => {
+  const chat = new ChatOpenAI(
+    {
+      modelName: "gpt-3.5-turbo",
+      temperature: 0.7,
+      cache: true,
+      concurrency: 5,
+      verbose: true,
+      streaming: true,
+      callbackManager,
+    },
+    {
+      basePath: process.env.BASE_PATH,
+    }
+  );
 
-const callbackManager = CallbackManager.fromHandlers({
-  handleLLMNewToken: async (token: string) => {
-    currentMessage += token;
-    // console.log(currentMessage);
-  },
-  handleLLMStart: async (llm: { name: string }, prompts: string[]) => {
-    // console.log(JSON.stringify(llm, null, 2));
-    // console.log(JSON.stringify(prompts, null, 2));
-  },
-  handleLLMEnd: async (output: LLMResult) => {
-    // console.log(JSON.stringify(output, null, 2));
-  },
-  handleLLMError: async (err: Error) => {
-    console.error(err);
-  },
-});
+  const chatPrompt = ChatPromptTemplate.fromPromptMessages([
+    SystemMessagePromptTemplate.fromTemplate(
+      "你是一个拥有永久记忆的仿生人，你可以被命名并满足用户提出的任何要求。回复要求语言优美、人性化、句式丰富。回复时可以参考记忆，如果没有任何记忆则仅根据收到的消息回复。回答完毕后也可以根据记忆和当前对话发起提问。\n"
+    ),
+    new MessagesPlaceholder("history"),
+    HumanMessagePromptTemplate.fromTemplate("{input}"),
+  ]);
 
-const chat = new ChatOpenAI(
-  {
-    modelName: "gpt-3.5-turbo",
-    temperature: 0.7,
-    cache: true,
-    concurrency: 5,
-    verbose: true,
-    streaming: true,
-    callbackManager,
-  },
-  {
-    basePath: process.env.BASE_PATH,
+  const chain = new ConversationChain({
+    memory: new BufferWindowMemory({
+      k: 3,
+      returnMessages: true,
+      memoryKey: "history",
+    }),
+    prompt: chatPrompt,
+    llm: chat,
+  });
+
+  // Save the vector store to a directory
+  const directory = `data/${userId}`;
+
+  let loadedVectorStore: HNSWLib | null = null;
+
+  let memory = "";
+
+  if (fs.existsSync(directory)) {
+    // Load the vector store from the same directory
+    loadedVectorStore = await HNSWLib.load(directory, new OpenAIEmbeddings());
+    const searchResults = await loadedVectorStore.similaritySearch(question, 3);
+
+    memory = searchResults.reduce((acc, curr) => acc + curr.pageContent, "");
+  } else {
+    console.log("Directory not found.");
   }
-);
 
-const chatPrompt = ChatPromptTemplate.fromPromptMessages([
-  SystemMessagePromptTemplate.fromTemplate(
-    "你是一个拥有永久记忆的仿生人，你可以被命名并满足用户提出的任何要求。回复要求语言优美、人性化、句式丰富。回复时可以参考记忆，如果没有任何记忆则仅根据收到的消息回复。回答完毕后也可以根据记忆和当前对话发起提问。\n"
-  ),
-  new MessagesPlaceholder("history"),
-  HumanMessagePromptTemplate.fromTemplate("{input}"),
-]);
+  console.log("memory: ", memory);
 
-const chain = new ConversationChain({
-  memory: new BufferWindowMemory({
-    k: 3,
-    returnMessages: true,
-    memoryKey: "history",
-  }),
-  prompt: chatPrompt,
-  llm: chat,
-});
+  const response = await chain.call({
+    input: `你关于此段对话的记忆是: ${memory} \n当前消息: ${question}`,
+  });
+  const filterMessage = response.replace("AI: ", "");
+  console.log("response: ", response);
+
+  const docs = [
+    new Document({
+      metadata: { id: uuidv4(), date: new Date().toISOString() },
+      pageContent: `Human:${question}AI:${filterMessage}`,
+    }),
+  ];
+
+  // console.log(docs);
+
+  if (fs.existsSync(directory) && loadedVectorStore) {
+    await loadedVectorStore.addDocuments(docs);
+
+    await loadedVectorStore.save(directory);
+  } else {
+    const vectorStore = await HNSWLib.fromDocuments(
+      docs,
+      new OpenAIEmbeddings()
+    );
+    await vectorStore.save(directory);
+  }
+};
 
 // Telegram bot
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN as string);
 
 bot.start((ctx) => {
-  ctx.reply(
-    "Hello, this is a bot that uses OpenAI.\nAsk anything using /ask followed by your question."
-  );
+  ctx.reply("Hello, this is a bot that uses OpenAI with forever memory.");
 });
 
 bot.on(message("text"), async (ctx) => {
-  currentMessage = "";
-  replyedMessage = "";
-
   const userId = ctx.update.message.from.id;
 
   // console.log("userId: ", userId);
@@ -112,7 +138,7 @@ bot.on(message("text"), async (ctx) => {
   const question = ctx.update.message.text.trim();
 
   if (question.length == 0) {
-    return ctx.reply("Type something after /ask to ask me stuff.", {
+    return ctx.reply("Type something to ask me stuff.", {
       reply_to_message_id: ctx.message.message_id,
     });
   }
@@ -120,101 +146,57 @@ bot.on(message("text"), async (ctx) => {
   ctx.sendChatAction("typing");
 
   try {
-    const initReply = await ctx.reply(marked.parseInline(`... \n`), {
+    //create our callback manager
+    let throttle = false;
+    let replyedMessage = "";
+    let editMessage: any = await ctx.reply(marked.parseInline(`... \n`), {
       reply_to_message_id: ctx.message.message_id,
       parse_mode: "HTML",
     });
 
-    // Save the vector store to a directory
-    const directory = `data/${userId}`;
-
-    let loadedVectorStore: HNSWLib | null = null;
-
-    let memory = "";
-
-    if (fs.existsSync(directory)) {
-      // Load the vector store from the same directory
-      loadedVectorStore = await HNSWLib.load(directory, new OpenAIEmbeddings());
-      const searchResults = await loadedVectorStore.similaritySearch(
-        question,
-        3
-      );
-
-      memory = searchResults.reduce((acc, curr) => acc + curr.pageContent, "");
-    } else {
-      console.log("Directory not found.");
-    }
-
-    console.log("memory: ", memory);
-
-    const updateInterval = setInterval(async () => {
-      // console.log("currentMessage: ", currentMessage);
-      // console.log("replyedMessage: ", replyedMessage);
-      if (currentMessage !== replyedMessage) {
-        try {
-          const editMessage = await ctx.telegram.editMessageText(
-            ctx.chat.id,
-            initReply.message_id,
-            "0",
-            currentMessage.replace("AI: ", "")
-          );
-          if ((editMessage as any).text) {
-            replyedMessage = (editMessage as any).text;
-          }
-        } catch (error) {
-          console.log(error);
-        }
-      }
-    }, 1000);
-
-    setTimeout(() => {
-      clearInterval(updateInterval);
-    }, 1000 * 30);
-
-    chain
-      .call({
-        input: `你关于此段对话的记忆是: ${memory} \n当前消息: ${question}`,
-      })
-      .then(async ({ response }) => {
-        const filterMessage = response.replace("AI: ", "");
-        console.log("response: ", response);
-
-        clearInterval(updateInterval);
-
-        if (replyedMessage !== filterMessage) {
+    const callbackManager = CallbackManager.fromHandlers({
+      handleLLMNewToken: async (token: string) => {
+        replyedMessage += token;
+        if (!throttle && replyedMessage) {
+          throttle = true;
           try {
-            await ctx.telegram.editMessageText(
+            editMessage = await ctx.telegram.editMessageText(
               ctx.chat.id,
-              initReply.message_id,
+              editMessage.message_id,
               "0",
-              filterMessage
+              replyedMessage
             );
+            setTimeout(() => {
+              throttle = false;
+            }, 1000);
           } catch (error) {
             console.log(error);
           }
         }
-
-        const docs = [
-          new Document({
-            metadata: { id: uuidv4(), date: new Date().toISOString() },
-            pageContent: `Human:${question}AI:${filterMessage}`,
-          }),
-        ];
-
-        // console.log(docs);
-
-        if (fs.existsSync(directory) && loadedVectorStore) {
-          await loadedVectorStore.addDocuments(docs);
-
-          await loadedVectorStore.save(directory);
-        } else {
-          const vectorStore = await HNSWLib.fromDocuments(
-            docs,
-            new OpenAIEmbeddings()
+      },
+      handleLLMStart: async (llm: { name: string }, prompts: string[]) => {
+        // console.log(JSON.stringify(llm, null, 2));
+        // console.log(JSON.stringify(prompts, null, 2));
+      },
+      handleLLMEnd: async (output: LLMResult) => {
+        // console.log(JSON.stringify(output, null, 2));
+        try {
+          editMessage = await ctx.telegram.editMessageText(
+            ctx.chat.id,
+            editMessage.message_id,
+            "0",
+            output.generations[0][0].text
           );
-          await vectorStore.save(directory);
+        } catch (error) {
+          console.log(error);
         }
-      });
+      },
+      handleLLMError: async (err: Error) => {
+        console.error(err);
+      },
+    });
+
+    await run(userId, question, callbackManager);
 
     // console.log(res.response);
   } catch (error) {
